856ac914cf864d977a2ac68abc2b2f8cc9808eed
Sridhar <sreedharriyer@gmail.com>
2018-12-22 15:43:22 +0530

    Added myBatis

--- school/src/main/java/com/education/school/SchoolApplication.java
+++ school/src/main/java/com/education/school/SchoolApplication.java
@@ -1,9 +1,14 @@
 package com.education.school;
 
+import com.education.school.entity.Student;
+import org.apache.ibatis.type.MappedTypes;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@MappedTypes(Student.class)
+@MapperScan("com.eduction.school.mapper")
 @SpringBootApplication
 @EnableSwagger2
 public class SchoolApplication {
