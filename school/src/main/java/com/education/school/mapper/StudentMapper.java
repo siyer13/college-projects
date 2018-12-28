33a0e61c6a5d659aafe564fbfc973662d27ba8df
Sridhar <sreedharriyer@gmail.com>
2018-12-28 21:42:58 +0530

    Student Mapper

--- school/src/main/java/com/education/school/mapper/StudentMapper.java
+++ school/src/main/java/com/education/school/mapper/StudentMapper.java
@@ -1,10 +1,9 @@
 package com.education.school.mapper;
 
+import com.education.school.entity.FinalResult;
 import com.education.school.entity.Student;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
+import com.education.school.exception.StudentNotFoundException;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -22,4 +21,19 @@ public interface StudentMapper {
             @Result(property = "course", column = "course")
     })
     List<Student> findAllStudents();
+
+    @Select("select * from ")
+    List getAllStudentResults();
+
+    @Select("select * from student where student_id = #{studentId}")
+    @Results(value = {
+            @Result(property = "studentId", column = "student_id"),
+            @Result(property = "firstName", column = "first_name"),
+            @Result(property = "lastName", column = "last_name"),
+            @Result(property = "middleName", column = "middle_name"),
+            @Result(property = "college", column = "college"),
+            @Result(property = "department", column = "department"),
+            @Result(property = "course", column = "course")
+    })
+    Student findStudentById(@Param("studentId") String studentId) throws StudentNotFoundException;
 }
