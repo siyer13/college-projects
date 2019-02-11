5d18695e6042568f3df0bc71fc56c508ed51894e
Sridhar <sreedharriyer@gmail.com>
2019-02-11 19:11:22 +0530

    DAAS - Database as Service

---
+++ DAAS/src/main/java/com/db/DAAS/DaasApplication.java
@@ -0,0 +1,14 @@
+package com.db.DAAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaasApplication.class, args);
+	}
+
+}
+
